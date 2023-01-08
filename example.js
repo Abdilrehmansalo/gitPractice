b806b08750b5de5a5bdc8f57d6e9c5a3526b0ea8
Abdilrehmansalo <120518791+Abdilrehmansalo@users.noreply.github.com>
2023-01-08 23:14:17 +0300

    update

--- example.js
+++ example.js
@@ -1,6 +1,15 @@
-var un={
-  name:"tarik",id:234,card:function(){
+var un = {
+  name: "tarik", id: 234, card: function () {
     console.log("hi student")
   }
 }
 console.log(un)
+
+let sum=function(x,y){
+  let add;
+  add=x+y;
+  return add
+}
+
+module.exports.sum=sum;
+console.log(module);
---
+++ example2.js
@@ -0,0 +1,26 @@
+// let work=function(f,s){
+
+//   let workDone;
+//   workDone=f*s
+// return workDone;
+
+// }
+// let sira =work(3,10);
+// console.log(sira)
+// console.log(module);
+// let read=()=>{
+//   console.log("hi everybody")
+// }
+// read();
+
+// let amta=require("./example");
+// console.log(amta);
+// console.log(amta.sum(1000,187));
+// console.log(global);
+// console.log(global.console); 
+function eat() {
+  console.log("Hana ate");
+  }
+  module.exports.e = eat; 
+  console.log(module.exports);
+  
